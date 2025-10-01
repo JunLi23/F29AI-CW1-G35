8928c3168f6cd59b4614df1184a7da931af0941f
JunLi <jl2123@hw.ac.uk>
2025-10-01 16:10:09 +0100

    Start outlining Sudoku Solver

--- Sudoku Solver/SolverMain.java
+++ Sudoku Solver/SolverMain.java
@@ -1,5 +1,59 @@
 //Main file for Sudoku Solver
 
-import java.util.Scanner;
+public static void main(){
+
+//read file, put into grid 2-d array
+
+int sudokuUnsolved[][]
+
+for (int i = 1; i = 1 to 8; i++) {
+
+}
+
+public class CheckRow{
+    //return boolean true or false
+    
+    boolean rowSolvable 
+    int currentCell
+
+    for (int i = 1; i = 1 to 9; i++) {
+        if currentCell
+
+    }
+
+}
+
+public class CheckCol{
+    //return boolean true or false
+
+    boolean colSolvable
+
+    for (int j = 1; j = 1 to 9; j++) {
+
+    }
+
+}
+
+    
+public class Check3x3{
+    //return boolean true or false
+
+    boolean 3x3Solvable
+
+    // to check 3x3 grid, check if coordinate is divisible by 3 using mod = 0
+
+    for (int i = 0; i = 0 to 2; i++)
+        for (int j = 0; j = 0 t0 2; j++){
+
+        }
+
+}
+
+
+static class SudokuSolver{
+
+
+
+}
 
 

10ca63ecd991840dcab38352959164657ca573c6
JunLi <jl2123@hw.ac.uk>
2025-10-01 16:11:41 +0100

    fix

--- src/main/Sudoku Solver/SolverMain.java
+++ src/main/Sudoku Solver/SolverMain.java
@@ -1,6 +1,6 @@
 //Main file for Sudoku Solver
 
-public static void main(){
+public static void main() {
 
 //read file, put into grid 2-d array
 
@@ -13,11 +13,11 @@ for (int i = 1; i = 1 to 8; i++) {
 public class CheckRow{
     //return boolean true or false
     
-    boolean rowSolvable 
-    int currentCell
+    boolean rowSolvable;
+    int currentCell;
 
     for (int i = 1; i = 1 to 9; i++) {
-        if currentCell
+        //if currentCell;
 
     }
 
@@ -38,12 +38,12 @@ public class CheckCol{
 public class Check3x3{
     //return boolean true or false
 
-    boolean 3x3Solvable
+    boolean 3x3Solvable;
 
     // to check 3x3 grid, check if coordinate is divisible by 3 using mod = 0
 
     for (int i = 0; i = 0 to 2; i++)
-        for (int j = 0; j = 0 t0 2; j++){
+        for (int j = 0; j = 0; 2; j++){
 
         }
 
